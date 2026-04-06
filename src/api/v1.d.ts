e899da4121543573f43636ac99124a4fbe3bbfcd
Lukas Wolfsberger <1245519+wolfsblu@users.noreply.github.com>
2026-04-06 14:54:32 +0200

    feat(api): add import rules API

--- src/api/v1.d.ts
+++ src/api/v1.d.ts
@@ -320,6 +320,40 @@ export interface paths {
         patch?: never;
         trace?: never;
     };
+    "/api/v1/transactions/import/rules": {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        get: {
+            parameters: {
+                query?: never;
+                header?: never;
+                path?: never;
+                cookie?: never;
+            };
+            requestBody?: never;
+            responses: {
+                200: {
+                    headers: {
+                        [name: string]: unknown;
+                    };
+                    content: {
+                        "application/json;charset=utf-8": string[];
+                    };
+                };
+            };
+        };
+        put?: never;
+        post?: never;
+        delete?: never;
+        options?: never;
+        head?: never;
+        patch?: never;
+        trace?: never;
+    };
     "/api/v1/transactions/import/{rules}": {
         parameters: {
             query?: never;
